77d03efdcfacc06ee45609520c7cc6bd08fa2408
Nilaksh Parihar <pariharnilaksh@gmail.com>
2025-02-17 23:44:52 +0530

    Updating tic-tac-toe

--- Tic-Tac-Toe/tictactoescript.js
+++ Tic-Tac-Toe/tictactoescript.js
@@ -1,13 +1,32 @@
 console.log("Tic-Tac-Toe Script is working");
-let music = new Audio("music.mp3");
-let turn = new Audio("metal-ting.mp3");
-let gameover = new Audio("gameover.mp3");
-let turn1 = "X"
+// let music = new Audio("music.mp3");
+let audioturn = new Audio("metal-ting.mp3");
+// let gameover = new Audio("gameover.mp3");
+let turn = "X"
 
 // Function to change the turn
 const changeTurn = () => {
-    return turn1 === "X" ? "0" : "X"
+    return turn === "X" ? "0" : "X"
 }
 
 // Function to check for a win
+const checkWin = () => {
+
+}
+
+//Game Logic
+let boxes = document.getElementsByClassName("Box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn =  changeTurn();
+            audioturn.play();
+            checkWin();
+            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+        }
+    })
+})
+
 
